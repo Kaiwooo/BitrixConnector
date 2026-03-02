c718d92db0c81c90b5f926eb36b42cb0e0c011c3
p.gerasimenko <p.gerasimenko@medcall.center>
2026-03-02 17:53:56 +0300

    1

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ CLIENT_ID = ""       # Для refresh_token, если нужно
 CLIENT_SECRET = ""   # Для refresh_token, если нужно
 DEBUG = True
 INSTALL_WEBHOOK = "https://bitrixconnector.onrender.com/install"
-EVENT_WEBHOOK = "https://bitrixconnector.onrender.com/event""
+EVENT_WEBHOOK = "https://bitrixconnector.onrender.com/event"
 CONFIG_FILE = "apps_config.json"
