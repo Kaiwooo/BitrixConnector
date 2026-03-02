a97d1e56ad9668d3083b989254616d6157c9afe3
p.gerasimenko <p.gerasimenko@medcall.center>
2026-03-02 17:56:47 +0300

    1

--- webhooks/connector_activate.py
+++ webhooks/connector_activate.py
@@ -1,1 +1,9 @@
-#connector_activate.py
+from fastapi import APIRouter, Request
+from Bitrix.call import call
+from storage import load_config
+
+router = APIRouter()
+
+@router.post("")
+async def register_connector(request: Request):
+    apps = load_config()
