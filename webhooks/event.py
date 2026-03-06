138e4d41d9bf8561c28b82565d645d831c306846
p.gerasimenko <p.gerasimenko@medcall.center>
2026-03-06 16:16:18 +0300

    new call.py - solved expired token

--- webhooks/event.py
+++ webhooks/event.py
@@ -10,7 +10,6 @@ async def handler_onimconnectormessageadd(data: dict):
     log_dict(logger, {"Inbound Event": data})
     connector_id = data.get("data[CONNECTOR]")
     line_id = data.get("data[LINE]")
-    message = data.get("data[MESSAGES][0][im][chat]")
     chat_id = data.get("data[MESSAGES][0][im][id]")
     message_id = data.get("data[MESSAGES][0][im][message_id]")
     params = {
@@ -19,10 +18,12 @@ async def handler_onimconnectormessageadd(data: dict):
         "MESSAGES[0][CHAT_ID]": chat_id,
         "MESSAGES[0][MESSAGE_ID]": message_id
     }
-    auth = data.get("AUTH[access_token]")
-    app_token = data.get("AUTH[app_token]")
-    result = await call("imconnector.send.status.delivery", params, auth, app_token)
-    return result
+    auth = {
+        "access_token": data.get("auth[access_token]"),
+        "client_endpoint": data.get("auth[client_endpoint]")
+    }
+    result = await call("imconnector.send.status.delivery", params, auth)
+    log_dict(logger, {"Delivery result": result})
 
 
 @router.post("")
