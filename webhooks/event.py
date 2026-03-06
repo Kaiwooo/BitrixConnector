098ba6981387cc6096cbd1f180fa1eb21aca7efb
p.gerasimenko <p.gerasimenko@medcall.center>
2026-03-06 16:02:18 +0300

    new call.py - solved expired token

--- webhooks/event.py
+++ webhooks/event.py
@@ -1,16 +1,28 @@
 import logging
 from fastapi import APIRouter, Request
 from utils.logging_helper import log_dict
+from client.call import call
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
 async def handler_onimconnectormessageadd(data: dict):
-    message = data.get("data[MESSAGES][0][message][text]")
-    chat_id = data.get("data[MESSAGES][0][chat][id]")
     log_dict(logger, {"Inbound Event": data})
+    connector_id = data.get("data[CONNECTOR]")
+    line_id = data.get("data[LINE]")
+    message = data.get("data[MESSAGES][0][im][chat]")
+    chat_id = data.get("data[MESSAGES][0][im][id]")
+    message_id = data.get("data[MESSAGES][0][im][message_id]")
+    params = {
+        "CONNECTOR": connector_id,
+        "LINE": line_id,
+        "MESSAGES[0][CHAT_ID]": chat_id,
+        "MESSAGES[0][MESSAGE_ID]": message_id
+    }
+    auth = data.get("AUTH[access_token]")
+    app_token = data.get("AUTH[app_token]")
     if message and chat_id:
-        logger.info(f"Новое сообщение: {message} | chat: {chat_id}")
+        await call("imconnector.send.status.delivery", params, auth, app_token)
     return
 
 
